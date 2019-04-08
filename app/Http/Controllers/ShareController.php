6a300047f7a03a07851327759538f0eb6b23d3df
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-04-08 15:03:37 +0800

    Edit, Update, and Delete Data functions

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -73,7 +73,10 @@ class ShareController extends Controller
      */
     public function edit($id)
     {
-        //
+        // added by me
+        $share = Share::find($id);
+
+        return view('shares.edit', compact('share'));
     }
 
     /**
@@ -85,7 +88,20 @@ class ShareController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // added by me
+        $request->validate([
+            'share_name' => 'required',
+            'share_price' => 'required|integer',
+            'share_qty' => 'required|integer'
+        ]);
+
+        $share = Share::find($id);
+        $share->share_name = $request->get('share_name');
+        $share->share_price = $request->get('share_price');
+        $share->share_qty = $request->get('share_qty');
+        $share->save();
+
+        return redirect('/shares')->with('success', 'Stock has been updated');
     }
 
     /**
@@ -96,6 +112,10 @@ class ShareController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // added by me
+        $share = Share::find($id);
+        $share->delete();
+
+        return redirect('/shares')->with('success', 'Stock has been deleted successfully');
     }
 }
